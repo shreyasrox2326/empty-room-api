68c76a5c004ee38550f9cf4329391305bfcd80df
shreyasrox2326 <110967852+shreyasrox2326@users.noreply.github.com>
2025-07-04 14:11:57 +0530

    Update roomapi.py

--- roomapi.py
+++ roomapi.py
@@ -24,11 +24,13 @@ def home():
         if key != xxxx: sleep(3)
 
         data = [
-            {"Action": "Help", "Format" : "/?no-delay-key=--KEY--"},
-            {"Action": "List all rooms", "Format" : "/list-rooms?no-delay-key=--KEY--"},
-            {"Action": "Fetch data for room", "Format" : "/fetch-room?roomname=D217?no-delay-key=--KEY--"},
-            {"Action" : "Check if room is available during a given instant on a specific day", "Format" : "/check-instant?roomname=D217&day=Mon&time=01:00 PM?no-delay-key=--KEY--"},
-            {"Action" : "Check if room is available during a given interval on a specific day", "Format" : "/check-interval?roomname=D217&day=Mon&starttime=09:00 AM&endtime=01:00 PM?no-delay-key=--KEY--"}
+            {"Action": "Help", "Format" : "/?no-delay-key=--OPTIONAL--KEY--"},
+            {"Action": "List all rooms", "Format" : "/list-rooms&no-delay-key=--OPTIONAL--KEY--"},
+            {"Action": "Fetch data for room", "Format" : "/fetch-room?roomname=D217&no-delay-key=--OPTIONAL--KEY--"},
+            {"Action" : "Check if room is available during a given instant on a specific day", "Format" : "/check-instant?roomname=D217&day=Mon&time=01:00 PM&no-delay-key=--OPTIONAL--KEY--"},
+            {"Action" : "Check if room is available during a given interval on a specific day", "Format" : "/check-interval?roomname=D217&day=Mon&starttime=09:00 AM&endtime=01:00 PM&no-delay-key=--OPTIONAL--KEY--"},
+            {"Action" : "List all available rooms during a given instant on a specific day", "Format" : "/list-instant?day=Mon&time=01:00 PM&no-delay-key=--OPTIONAL--KEY--"},
+            {"Action" : "List all available rooms during a given interval on a specific day", "Format" : "/list-interval?day=Mon&starttime=09:00 AM&endtime=01:00 PM&no-delay-key=--OPTIONAL--KEY--"},
         ]
         return jsonify(data)
     
@@ -77,8 +79,31 @@ def check_interval():
         data = fetchroom(roomname).checkinterval(day, starttime, endtime)
         return jsonify({"roomname":roomname, "day" : day, 'interval': starttime+' - '+endtime, 'available' : data})
     
-    
 
+@app.route('/list-instant', methods = ['GET', 'POST'])
+def list_instant():
+    if(request.method == 'GET'):
+        day = request.args.get("day")
+        time = request.args.get("time")
+        key = request.args.get('no-delay-key', -1)
+        if key != xxxx: sleep(3)
+
+        data = [i for i in csv_result['roomset'] if fetchroom(i).checkinstant(day, time)]
+        return jsonify({"day" : day, 'time': time, 'available' : data})
+
+@app.route('/list-interval', methods = ['GET', 'POST'])
+def list_interval():
+    if(request.method == 'GET'):
+        day = request.args.get("day")
+        starttime = request.args.get("starttime")
+        endtime = request.args.get("endtime")
+
+        key = request.args.get('no-delay-key', -1)
+        if key != xxxx: sleep(3)
+
+        
+        data = [i for i in csv_result['roomset'] if fetchroom(i).checkinterval(day, starttime, endtime)]
+        return jsonify({"day" : day, 'interval': starttime+' - '+endtime, 'available' : data})
 
 # Run with gunicorn -w 1 roomapi:app
 
